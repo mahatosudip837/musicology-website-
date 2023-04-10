892976424bd361f6918d6d96d307f19b02af1c96
Sudip Mahato <mahatosudip837@gmail.com>
2023-04-10 15:18:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelectorAll(".nav-links li");
+
+// hamburger.addEventListener('click', ()=>{
+//     //Animate Links
+//      navLinks.classList.toggle("open");
+//      links.forEach(link => {
+//          link.classList.toggle("fade");
+//      });
+ 
+//      //Hamburger Animation
+//      hamburger.classList.toggle("toggle");
+//  });
+
+
+ const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelectorAll("header ul li");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
